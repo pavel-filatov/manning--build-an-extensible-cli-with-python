9d2bf171980c3096857eb7e34370781276ae27c3
Pavel Filatov <filatov.py@gmail.com>
2022-08-21 11:50:24 +0200

    Apply pre-commit hooks

--- tests/test_klickbrick.py
+++ tests/test_klickbrick.py
@@ -4,6 +4,5 @@ from klickbrick import __version__
 
 
 class TestKlickbrick(unittest.TestCase):
-
     def test_version(self):
-        assert __version__ == '0.1.0'
+        assert __version__ == "0.1.0"
